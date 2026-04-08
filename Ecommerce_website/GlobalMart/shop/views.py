a63e5ee8fdabef0b0b5ed6c42aa332ca852f5fa9
Raghav007-maker <lalitaagrawal3321@gmail.com>
2026-04-08 16:35:14 +0530

    creation of blog and shop app in project

--- Ecommerce_website/GlobalMart/shop/views.py
+++ Ecommerce_website/GlobalMart/shop/views.py
@@ -6,5 +6,27 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Index Shop")
+    return render(request, 'shop/index.html')
+
+def about(request):
+    return HttpResponse("We are in about")
+
+def contact(request):
+    return HttpResponse("We are in contact")
+
+
+def tracker(request):
+    return HttpResponse("We are in tracker")
+
+def search(request):
+    return HttpResponse("We are in contact")
+
+def productView(request):
+    return HttpResponse("We are in productview")
+
+def checkOut(request):
+    return HttpResponse("We are going to do checkout")
+
+
+
 
