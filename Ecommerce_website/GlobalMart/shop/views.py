0a767386e0ff293bb0e45202dcd1d2a53e182aab
Raghav bansal <lalitaagrawal3321@gmail.com>
2026-04-25 19:37:42 +0530

    updated database and added new images for the web

--- Ecommerce_website/GlobalMart/shop/views.py
+++ Ecommerce_website/GlobalMart/shop/views.py
@@ -2,14 +2,22 @@
 
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Product
+from math import ceil
 # Create your views here.
 
 def index(request):
-    return render(request, 'shop/index.html')
+    products = Product.objects.all()
+    n = len(products)
+
+    # Keep 4 products per carousel slide.
+    nSlides = ceil(n / 4) if n else 1
+    params = {'no_of_slides': nSlides, 'range': range(1, nSlides), 'product': products}
+    return render(request,"shop/index.html", params)
 
 def about(request):
-    return HttpResponse("We are in about")
+    return render(request, 'shop/about.html')
+    
 
 def contact(request):
     return HttpResponse("We are in contact")
